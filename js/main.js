3260a6ca264219bc71db1e9840032208d804ad03
tagasouichiro <stitow14@gmail.com>
2018-04-08 23:51:33 +0900

    Add search request

--- js/main.js
+++ js/main.js
@@ -1,5 +1,17 @@
 
-function alertTest() {
-  alert('hoge')
-  console.log('ALEART')
+function searchChiken() {
+  const word = document.chikenSearch.word.value
+  console.log(word)
+
+  const request = new XMLHttpRequest()
+  request.open("GET", ``)
+  request.addEventListener("load", (event) => {
+    console.log(event.target.status)
+    console.log(event.target.responseText)
+  })
+  request.addEventListener("error", (event) => {
+    console.log(event.target.status)
+    console.log("Error")
+  })
+  request.send()
 }
