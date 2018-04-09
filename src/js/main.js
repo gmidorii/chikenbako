c2ef83c75fb3b87f9e2d1b26394790e71cd60bc2
tagasouichiro <stitow14@gmail.com>
2018-04-10 00:14:43 +0900

    Fix

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,11 @@
 
-const url = 'http://localhost:8888'
+const apiUrl = 'http://localhost:8888'
 
 function searchChiken() {
   const word = document.chikenSearch.word.value
 
   const request = new XMLHttpRequest()
-  request.open("GET", `${url}/search?word=${word}`)
+  request.open("GET", `${apiUrl}/search?word=${word}`)
   request.addEventListener("load", (event) => {
     console.log(event.target.status)
     console.log(event.target.responseText)
