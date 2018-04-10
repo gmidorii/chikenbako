2b15036ec86b16616a99dc2ca19a3ea5fed92f53
tagasouichiro <stitow14@gmail.com>
2018-04-11 00:31:40 +0900

    Fix

--- src/js/main.js
+++ src/js/main.js
@@ -1,18 +1,19 @@
-
 const apiUrl = 'http://localhost:8888'
 
-function searchChiken() {
+const searchChiken = async () => {
   const word = document.chikenSearch.word.value
 
-  const request = new XMLHttpRequest()
-  request.open("GET", `${apiUrl}/search?word=${word}`)
-  request.addEventListener("load", (event) => {
-    console.log(event.target.status)
-    console.log(event.target.responseText)
-  })
-  request.addEventListener("error", (event) => {
-    console.log(event.target.status)
-    console.log("Error")
-  })
-  request.send()
+  try {
+    const res = await fetch(`${apiUrl}/search?word=${word}`)
+    const json = await res.json()
+    console.log(json)
+  } catch (e) {
+    console.log(e)
+    return
+  }
+
+}
+
+export {
+  searchChiken
 }
