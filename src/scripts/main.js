cf97332b021b55f39c43f1fd6e1012c4a0aabd5e
tagasouichiro <stitow14@gmail.com>
2018-04-14 13:02:18 +0900

    Fix

---
+++ src/scripts/main.js
@@ -0,0 +1,15 @@
+const apiUrl = 'http://localhost:8888'
+
+function searchChiken() {
+  console.log('hello')
+  // const word = document.chikenSearch.word.value
+
+  // try {
+  //   const res = await fetch(`${apiUrl}/search?word=${word}`)
+  //   const json = await res.json()
+  //   console.log(json)
+  // } catch (e) {
+  //   console.log(e)
+  //   return
+  // }
+}
