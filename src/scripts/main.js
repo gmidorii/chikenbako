f8eb2e6e3b7e19b66acfbb99119bf981f3fe905a
midorigreen <stitow14@gmail.com>
2018-04-18 22:11:25 +0900

    Fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,17 +1,16 @@
 const apiUrl = 'http://localhost:8888'
 
 function searchChiken() {
-  console.log('hello')
-  // const word = document.chikenSearch.word.value
+  const word = document.chikenSearch.word.value
 
-  // try {
-  //   const res = await fetch(`${apiUrl}/search?word=${word}`)
-  //   const json = await res.json()
-  //   console.log(json)
-  // } catch (e) {
-  //   console.log(e)
-  //   return
-  // }
+  try {
+    const res = await fetch(`${apiUrl}/search?word=${word}`)
+    const json = await res.json()
+    console.log(json)
+  } catch (e) {
+    console.log(e)
+    return
+  }
 }
 
 window.searchChiken = searchChiken
